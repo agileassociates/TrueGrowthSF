cacc5f26a77eefc1e4623b10f4c4a91d5d7e13a2
johny babylon <ztredded@gmail.com>
2016-02-18 22:33:07 -0800

    Finished Login

--- TrueGrowthSF/ViewController.m
+++ TrueGrowthSF/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -25,7 +26,78 @@
 }
 
 - (IBAction)signinClicked:(id)sender {
-    [self performSegueWithIdentifier:@"login_success" sender:self];
+    
+    if([[self.emailTextField text] isEqualToString:@""] || [[self.passwordTextField text] isEqualToString:@""] ) {
+        
+                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Login Failed"
+                                                        message:@"Please enter Email & Password"
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+                    [alert show];
+
+        
+            }
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    AFSecurityPolicy *policy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
+    policy.allowInvalidCertificates = YES;
+    manager.securityPolicy = policy;
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
+    NSString *email = _emailTextField.text;
+    NSString *password = _passwordTextField.text;
+    
+    
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"email"] = email;
+    params[@"password"] = password;
+    
+    [manager POST:@"https://127.0.0.1:9292/api/sessions" parameters:params progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+        
+        if ( responseObject[@"errors"] == NULL){
+        
+        
+        NSLog(@"%@", responseObject[@"data"][@"attributes"][@"auth_token"]);
+        
+        [self performSegueWithIdentifier:@"login_success" sender:self];
+            
+        } else {
+            
+            UIAlertController * alert=   [UIAlertController
+                                          alertControllerWithTitle:@"Info"
+                                          message:responseObject[@"errors"]
+                                          preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertAction* ok = [UIAlertAction
+                                 actionWithTitle:@"OK"
+                                 style:UIAlertActionStyleDefault
+                                 handler:^(UIAlertAction * action)
+                                 {
+                                     [alert dismissViewControllerAnimated:YES completion:nil];
+                                     
+                                 }];
+            
+            [alert addAction:ok];
+            
+            [self presentViewController:alert animated:YES completion:nil];
+            
+            NSLog(@"JSON: %@", responseObject[@"errors"]);
+
+            
+        }
+
+        
+        
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        
+        NSLog(@"error = %@", error);
+        
+    }];
+
 
     
     
